6ea36309ca9a64c738f8dcd2d156620f1d5bb01e
Sljivic Emina <if170106@WS8A12.local>
2019-02-11 08:50:07 +0100

    started with javascript

---
+++ Project/javascript/webapp.js
@@ -0,0 +1,12 @@
+var products=['iPhone','Mac','AppleWatch','AppleTV','iPad'];
+
+var year1;
+var year2;
+var x = document.getElementById("Products");
+
+console.log(document.getElementById("Products"));
+
+if(document.getElementById("Products")!==0)
+{
+  x.style.display='none';
+}

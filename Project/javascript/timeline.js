013b25edd4bcad5e94eb41deee1e1b2cb3961afb
Sljivic Emina <if170106@WS8A06.local>
2019-03-25 08:38:54 +0100

    wip ^^

--- Project/javascript/timeline.js
+++ Project/javascript/timeline.js
@@ -3,15 +3,15 @@ var lastYear='1976';
 function showYear(obj, id){
   document.getElementById(lastYear).style.cssText='display: none !important';
   document.getElementById(id).style.cssText='display: block !important';
-  var margin =  obj*2.983+1.9;
-  document.getElementById('triangleYear').style.cssText='margin-left:' + margin +'%!important; display: block !important';
+  var margin =  document.getElementById(obj).offsetLeft-5;
+  document.getElementById('triangleYear').style.cssText='margin-left:' + margin +'px!important; display: block !important';
   lastYear=id;
 }
 
 function hoverYear(obj, text){
   document.getElementById('hoverText').innerHTML = text;
-  var margin =  obj*2.983+1.25;
-  document.getElementById('hoverYear').style.cssText='margin-left:' + margin +'%!important; display: block !important';
+  var margin =  document.getElementById(obj).offsetLeft-15;
+  document.getElementById('hoverYear').style.cssText='margin-left:' + margin +'px!important; display: block !important';
 }
 
 function hideYear() {
